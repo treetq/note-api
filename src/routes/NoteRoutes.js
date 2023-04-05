e39be17ea3987fae7b43ea610eadba5ea79aa876
treeteq <treetechsaves@gmail.com>
2023-04-05 13:35:45 +0100

    implement note put and delete

--- src/routes/NoteRoutes.js
+++ src/routes/NoteRoutes.js
@@ -1,3 +1,4 @@
+const { json } = require("body-parser");
 const { NoteModel } = require("../db");
 
 module.exports = (app) => {
@@ -17,7 +18,6 @@ module.exports = (app) => {
     }
 
     // USE THIS METHOD
-
     /* const note = new NoteModel({
          title,
          content
@@ -40,6 +40,78 @@ module.exports = (app) => {
     return res.status(200).json({
       message: "Notes retrieved",
       data: notes,
+    });
+  });
+
+  app.get("/note/:id", async (req, res, next) => {
+    const { id } = req.params;
+    const note = await NoteModel.findById(id);
+
+    if (!note) {
+      return res.status(404).json({
+        message: "Note not found",
+      });
+    }
+    return res.status(200).json({
+      message: "Note retrieved",
+      data: note,
+    });
+  });
+
+  app.put("/notes/:id", async (req, res, next) => {
+    const { id } = req.params;
+    const { title, content } = req.body;
+
+    if (!title && !content) {
+      return res.status(400).json({
+        message: "title or content are required",
+      });
+    }
+
+    if (title && title.trim().length > 50) {
+      return res.status(400).json({
+        message: "title is too long max 50 character",
+      });
+    }
+
+    const note = await NoteModel.findById(id);
+
+    if (!note) {
+      return res.status(404).json({
+        message: "Note not found",
+      });
+    }
+
+    if (title) note.title = title.trim();
+
+    if (content) note.content = content.trim();
+
+    await note.save();
+
+    return res.status(200).json({
+      message: "Note updated",
+      data: note,
+    });
+  });
+
+  app.delete("/notes/:id", async (req, res, next) => {
+    const { id } = req.params;
+
+    console.error(id);
+
+    let note = await NoteModel.findById(id);
+    if (!note) {
+      return res.status(404).json({
+        message: "Note not found",
+      });
+    }
+
+    //await note.deleteById(id);
+    note = await NoteModel.findByIdAndDelete(id);
+
+    return res.status(200).json({
+      message: "Note deleted",
+      data: note,
     });
   });
 };
